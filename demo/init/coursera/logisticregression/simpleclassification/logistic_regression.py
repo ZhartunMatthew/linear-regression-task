7411d301f03c565e7d30eace15222fca3e21468c
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-07-20 17:19:19 +0300

    Added simple logistic regression

--- demo/init/coursera/logisticregression/simpleclassification/logistic_regression.py
+++
@@ -1,6 +0,0 @@
-from demo.init.coursera.logisticregression.simpleclassification import logistic_regression_common as lrc
-import numpy as np
-
-
-def compute_cost_function(x_data, y_data, theta):
-    pass
